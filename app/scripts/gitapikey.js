96cd02d317519c66102c0da8e45981da7d84df1f
dylan-gregory <jdylangregory@gmail.com>
2017-02-02 17:22:11 -0500

    Got most of the API info needed

--- app/scripts/gitapikey.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = {
-  "token": "6bd401c84ab7fe428be96f17c3a10a2d20ed3a74"
-}
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,15 +1,15 @@
-var $ = window.$ = window.Jquery = require('jquery');
+var $ = require('jquery');
 var _ = require('underscore');
 var Handlebars = require('handlebars');
 var githubtoken = require('./gitapikey.js');
-require('bootstrap-sass');
+// require('bootstrap-sass');
 
 // Send auth token to github if token is provided
 //put it in gitIgnore file! ***************************
 if (githubtoken !== undefined) {
   $.ajaxSetup({
     headers: {
-      'Authorization': 'token' + githubtoken.token
+      'Authorization': 'token ' + githubtoken.token
     }
   });
 }
@@ -43,3 +43,79 @@ if (githubtoken !== undefined) {
 
 // $.ajax('https://api.github.com/users/dylan-gregory')
 // $.ajax('https://api.github.com/users/dylan-gregory/repos')
+
+
+$.ajax('https://api.github.com/users/dylan-gregory').done(function(info){
+  console.log(info);
+    var bioInfo = {
+        avatar: info.avatar_url,
+        email: info.email,
+        login: info.login,
+        location: info.location,
+        name: info.name,
+        bio: info.bio
+
+
+      }
+
+    displayBio(bioInfo);
+    // console.log(data);
+});
+
+function displayBio(bioInfo){
+  var source = $('#bio-temp').html();
+  var template = Handlebars.compile(source);
+
+  $('#left-bar').append(template(bioInfo));
+
+};
+
+
+
+// accessing repo API
+$.ajax('https://api.github.com/users/dylan-gregory/repos').done(function(data){
+  console.log(data);
+    var repoList = data;
+    displayRepos(repoList);
+    // console.log(data);
+});
+
+// console.log(data);
+
+function displayRepos(repoList){
+  var source = $('#repo-template').html();
+  var template = Handlebars.compile(source);
+
+  _.each(repoList, function(repo){
+
+    $('#repo-list').append(template(repo));
+    // $('#email-box').append(template(email));
+
+  })
+
+};
+
+//
+// $.ajax('https://api.github.com/users/dylan-gregory/').done(function(data){
+//   console.log(data);
+//     var bioInfo = data;
+//     displayRepos(bioInfo);
+//     // console.log(data);
+// })
+//
+// function displayRepos(bioInfo){
+//   var source = $('#email-temp').html();
+//   var template = Handlebars.compile(source);
+//
+//   _.each(bioInfo, function(context){
+//
+//       var context = {
+//
+//
+//       }
+//
+//     // $('#email-box').append(template(email));
+//
+//   })
+//
+// }
